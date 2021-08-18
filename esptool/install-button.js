d149d2dc3350147325fd1f5d802f4afa4c32fa22
3MHousing <89056286+3MHousing@users.noreply.github.com>
2021-08-18 14:05:58 +0700

    Add files via upload

--- esptool/install-button.js
+++ esptool/install-button.js
@@ -24,7 +24,7 @@ class t extends HTMLElement {
         )),
         e.name = "activate";
         const n = document.createElement("button");
-        if (n.innerText = "INSTALL",
+        if (n.innerText = "Cài đặt",
         e.append(n),
         "adoptedStyleSheets"in Document.prototype && "replaceSync"in CSSStyleSheet.prototype) {
             const e = new CSSStyleSheet;
